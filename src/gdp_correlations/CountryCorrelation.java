9563dfc31aca28b3b7d1569b8db512eb099bfe65
Brandon Forster <brandonforster@gmail.com>
2012-12-02 14:21:32 -0500

    Cleaned up repo and fixed merge conflict residue.

--- src/gdp_correlations/CountryCorrelation.java
+++ src/gdp_correlations/CountryCorrelation.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package gdp_correlations;
 
 import java.io.*;
@@ -24,30 +23,3 @@ public class CountryCorrelation {
 	}
 	
 }
-=======
-package gdp_correlations;
-
-import java.io.*;
-
-public class CountryCorrelation {
-	
-	int weo1;
-	int weo2;
-	double correlation;
-	
-	public CountryCorrelation(int a, int b, double r) {
-		
-		weo1 = a;
-		weo2 = b;
-		correlation = r;
-	}
-	
-	public void Print(PrintStream outfile) {
-		
-		outfile.println(weo1 + " " + weo2 + " " + correlation);
-		
-		
-	}
-	
-}
->>>>>>> FETCH_HEAD
