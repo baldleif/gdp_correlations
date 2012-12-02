ec0e91cda55dc14e69247c916fb246d87f4a1b59
Matthew Bald <booj3000@gmail.com>
2012-12-02 15:38:05 -0500

    Trying to merge

--- src/gdp_correlations/CountryCorrelation.java
+++ src/gdp_correlations/CountryCorrelation.java
@@ -23,29 +23,3 @@ public class CountryCorrelation {
 	}
 	
 }
-=======
-package gdp_correlations;
-
-import java.io.*;
-
-public class CountryCorrelation {
-	
-	int weo1;
-	int weo2;
-	double correlation;
-	
-	public CountryCorrelation(int a, int b, double r) {
-		
-		weo1 = a;
-		weo2 = b;
-		correlation = r;
-	}
-	
-	public void Print(PrintStream outfile) {
-		
-		outfile.println(weo1 + " " + weo2 + " " + correlation);
-		
-		
-	}
-	
-}
